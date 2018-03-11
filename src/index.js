7bc609bdbd330d0ff7b9703db90030795b0f2ea5
Fábio <fsmaiorano@gmail.com>
2018-03-11 13:31:31 -0300

    update: initial location

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
-const App = () => <View />;
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
-export default App;
+export default class App extends Component {
+  render() {
+    const { region } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            latitude: -27.2177659,
+            longitude: -49.6451598,
+            latitudeDelta: 0.0042,
+            longitudeDelta: 0.0031,
+          }}
+        />
+      </View>
+    );
+  }
+}
