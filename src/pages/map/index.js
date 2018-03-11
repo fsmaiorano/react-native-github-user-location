068c1b495b31c56cd08d491b5e566918f15b791c
Fábio <fsmaiorano@gmail.com>
2018-03-11 16:49:25 -0300

    update: init modal implementation

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -16,24 +16,31 @@ export default class App extends Component {
 
   state = {
     modalVisible: false,
+    region: {
+      latitude: -27.2177659,
+      longitude: -49.6451598,
+      latitudeDelta: 0.0042,
+      longitudeDelta: 0.0031,
+    },
   }
 
+  onRegionChange = (region) => {
+    this.setState({ region });
+  }
 
   render() {
+    console.tron.log(this.state.region);
     return (
       <View style={styles.container}>
         <MapView
           onLongPress={() => this.setState({ modalVisible: true })}
           provider={PROVIDER_GOOGLE}
+          onRegionChange={this.onRegionChange}
           style={styles.map}
-          region={{
-              latitude: -27.2177659,
-              longitude: -49.6451598,
-              latitudeDelta: 0.0042,
-              longitudeDelta: 0.0031,
-            }}
+          region={this.state.region}
         />
         <Modal
+
           animationType="slide"
           transparent={false}
           visible={this.state.modalVisible}
@@ -41,11 +48,17 @@ export default class App extends Component {
             alert('Modal has been closed.');
           }}
         >
-          <Text>Modal</Text>
-          <TouchableHighlight onPress={() => this.setState({ modalVisible: false })}>
-            <Text>Cancelar</Text>
-          </TouchableHighlight>
+          <View>
+            <View>
+              <Text>Modal</Text>
+              <TouchableHighlight onPress={() => this.setState({ modalVisible: false })}>
+                <Text>Cancelar</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+
         </Modal>
+
 
       </View>
     );
